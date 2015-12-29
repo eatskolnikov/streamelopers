a55f7e21264606b38c07d5a023678a4027c3763e
Enmanuel Toribio <eats007@gmail.com>
2015-12-28 21:28:37 -0400

    Adding maxresults

--- src/app/controllers/homecontroller.js
+++ src/app/controllers/homecontroller.js
@@ -6,7 +6,7 @@ app.controller("HomeCtrl", ['$scope', '$http',"$timeout",'$filter', function($sc
     autoplay: 1
   };
   this.youtubeVideoId = 'gbYeKK_xo6U';
-  var playlistsUrl = 'https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyCsH_dFYaU0Pvc2KZUE-I4LI8ppg3DmZKk&channelId=UCznWXigAvBa1ZtrgRmJGZgg&part=id,snippet,status';
+  var playlistsUrl = 'https://www.googleapis.com/youtube/v3/playlists?key=AIzaSyCsH_dFYaU0Pvc2KZUE-I4LI8ppg3DmZKk&channelId=UCznWXigAvBa1ZtrgRmJGZgg&part=id,snippet&maxResults=24';
   this.init = function(){
     $http.get(playlistsUrl).success(angular.bind(this, function(data){
       this.playlists = orderBy(data.items,'title', false);
