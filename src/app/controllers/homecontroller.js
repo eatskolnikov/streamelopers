445ea69fb70d5defb32aa4281673575a31df81e3
Enmanuel Toribio <eats007@gmail.com>
2016-01-09 20:59:10 -0400

    Added donors

--- src/app/controllers/homecontroller.js
+++ src/app/controllers/homecontroller.js
@@ -17,11 +17,10 @@ app.controller("HomeCtrl", ['$scope', '$http',"$timeout",'$filter', function($sc
     	download: true,
 	    header: true,
     	complete: angular.bind(this, function(results) {
-    		console.log(results.data);
-        console.log(this.donors);
-        console.log(this);
-
-        this.donors = orderBy(results.data,'Amount',false);
+        results.data.push({Name:"Jhonatas Miguel Rodríguez", Amount:20});
+        results.data.push({Name:"Jhuliano Skittberg Moreno", Amount:10});
+        results.data.push({Name:"Alberto Espinal Cruz", Amount:10});
+        this.donors = orderBy(results.data,'Amount',true);
     	})
     });
   };
