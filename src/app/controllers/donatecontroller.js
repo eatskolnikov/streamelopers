f967d0bb501b80272ee227f27e44f2fb5c8ba46d
Enmanuel Toribio <eats007@gmail.com>
2016-02-22 12:36:25 -0400

    added donation iframe

--- src/app/controllers/donatecontroller.js
+++ src/app/controllers/donatecontroller.js
@@ -1,7 +1,3 @@
 app.controller("DonateCtrl", [ '$window', '$scope', function($window,$scope)
 {
-  this.init = function(){
-    $window.location.href='https://donorbox.org/streamelopers';
-  };
-  this.init();
 }]);
